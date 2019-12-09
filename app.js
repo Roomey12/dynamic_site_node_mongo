05ea288832913c0797d0491a98c0e00b5e0b3f19
Roomey12 <54270364+Roomey12@users.noreply.github.com>
2019-12-09 18:12:17 +0200

    change half

--- app.js
+++ app.js
@@ -1,95 +1,64 @@
-const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const objectId = require("mongodb").ObjectID;
-   
+const express = require('express');
+const mongoose = require('mongoose');
+const objectId = require('mongodb').ObjectID;
+const User = require('./models/User');
+const mongo_uri =
+  'mongodb+srv://admin:admin@cluster0-cuh7a.mongodb.net/test?retryWrites=true&w=majority';
 const app = express();
 const jsonParser = express.json();
- 
-const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
- 
-let dbClient;
- 
-app.use(express.static(__dirname + "/public"));
- 
-mongoClient.connect(function(err, client){
-    if(err) return console.log(err);
-    dbClient = client;
-    app.locals.collection = client.db("usersdb").collection("users");
-    app.listen(5000, function(){//тут было 3000
-        console.log("Сервер ожидает подключения...");
-    });
+
+mongoose
+  .connect(mongo_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('Mongodb connected'))
+  .catch(err => console.log(err));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    res.send(users);
+  });
 });
- 
-app.get("/api/users", function(req, res){
-        
-    const collection = req.app.locals.collection;
-    collection.find({}).toArray(function(err, users){
-         
-        if(err) return console.log(err);
-        res.send(users)
-    });
-     
+app.get('/api/users/:id', (req, res) => {
+  User.findById(req.params.id, (err, user) => {
+    res.send(user);
+  });
 });
-app.get("/api/users/:id", function(req, res){
-        
-    const id = new objectId(req.params.id);
-    const collection = req.app.locals.collection;
-    collection.findOne({_id: id}, function(err, user){
-               
-        if(err) return console.log(err);
-        res.send(user);
-    });
+
+app.post('/api/users', jsonParser, (req, res) => {
+  User.create({
+    phone: req.body.phone,
+    name: req.body.name,
+    debt: req.body.debt
+  }).then(user => res.send(user));
 });
-   
-app.post("/api/users", jsonParser, function (req, res) {
-       
-    if(!req.body) return res.sendStatus(400);
-       
-    const userPhone = req.body.phone;
-    const userName = req.body.name;
-    const userDebt = req.body.debt;
-    const user = {phone: userPhone, name: userName, debt: userDebt};
-       
-    const collection = req.app.locals.collection;
-    collection.insertOne(user, function(err){//тут было unction(err, result)
-        
-        if(err) return console.log(err);
-        res.send(user);
-    });
+
+app.delete('/api/users/:id', function(req, res) {
+  User.findByIdAndRemove(req.params.id, (err, user) => {
+    res.send(user);
+  });
 });
-    
-app.delete("/api/users/:id", function(req, res){
-        
-    const id = new objectId(req.params.id);
-    const collection = req.app.locals.collection;
-    collection.findOneAndDelete({_id: id}, function(err, result){
-               
-        if(err) return console.log(err);    
-        let user = result.value;
-        res.send(user);
-    });
+
+app.put('/api/users/', jsonParser, function(req, res) {
+  if (!req.body)
+    return res.status(400).json({ error: 'You havent provided any data' });
+  console.log(req.body);
+
+  User.findOneAndUpdate(
+    { _id: req.body.id },
+    {
+      name: req.body.name,
+      phone: req.body.phone,
+      debt: req.body.debt
+    },
+    (err, user) => {
+      res.send(user);
+    }
+  );
 });
-   
-app.put("/api/users", jsonParser, function(req, res){
-        
-    if(!req.body) return res.sendStatus(400);
-    const id = new objectId(req.body.id);
-    const userPhone = req.body.phone;
-    const userName = req.body.name;
-    const userDebt = req.body.debt;
-       
-    const collection = req.app.locals.collection;
-    collection.findOneAndUpdate({_id: id}, { $set: {debt: userDebt, name: userName, phone: userPhone}},
-         {returnOriginal: false },function(err, result){
-               
-        if(err) return console.log(err);     
-        const user = result.value;
-        res.send(user);
-    });
-});
- 
-// прослушиваем прерывание работы программы (ctrl-c)
-process.on("SIGINT", () => {
-    dbClient.close();
-    process.exit();
-});
+
+app.listen(process.env.PORT || 3000, () => console.log('Server is running'));
